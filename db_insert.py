d7482475001ea10757a003ffcc2f8e0770de97c2
Seth Dudenhofer <sdudenhofer@gmail.com>
2025-09-24 17:30:05 -0500

    update to better use polars

--- db_insert.py
+++ db_insert.py
@@ -1,35 +1,26 @@
 import polars as pl
 from geopy.geocoders import Nominatim
 from sqlmodel import SQLModel, Field
-from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 geolocator = Nominatim(user_agent='dudenhofer_realestate', timeout=100)
 raw_data = "./data/City_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"
-reader = pl.read_csv(raw_data)
-data = pl.DataFrame(reader)
+q = (
+    pl.scan_csv(raw_data)       # lazy, no full load
+      .sort("State")  # predicate pushdown
+      .select(["State", "RegionName"]) # projection pushdown
+)
 
-
-def create_latlong():
-    for rows in data[['RegionName', 'State']].iter_rows(named=True):
+data = q.collect() 
+for rows in data.iter_rows(named=True):
         city_state = f"{rows['RegionName']}, {rows['State']}"
         location = geolocator.geocode(city_state)
-        try:
-            response = (
-                supabase.table("geoData")
-                .upsert({
-                    "city": rows['RegionName'],
-                    "state": rows['State'],
-                    "latitude": location.latitude if location else None,
-                    "longitude": location.longitude if location else None
-                }, ignore_duplicates=True)
-                .execute()
-            )
-            print(response)
-        except Exception as exception:
-            print(exception)
+        if location:
+            print(location.latitude, location.longitude)
+        else:
+            print(f"No Latitude, Longitude Data for: {city_state}")
 
-if __name__ == "__main__":
-    create_latlong()
+# if __name__ == "__main__":
+#    create_latlong()
