0e19f6917f4639f47737df8873de4ae229f11f9e
Seth Dudenhofer <sdudenhofer@gmail.com>
2025-09-27 17:04:35 -0500

    update to write to duckdb

--- db_insert.py
+++ db_insert.py
@@ -3,24 +3,48 @@ from geopy.geocoders import Nominatim
 from sqlmodel import SQLModel, Field
 from dotenv import load_dotenv
 import os
+import duckdb
+import pyarrow as pa
 
 load_dotenv()
+
+con = duckdb.connect('real_estate.db')
 geolocator = Nominatim(user_agent='dudenhofer_realestate', timeout=100)
 raw_data = "./data/City_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"
 q = (
-    pl.scan_csv(raw_data)       # lazy, no full load
-      .sort("State")  # predicate pushdown
-      .select(["State", "RegionName"]) # projection pushdown
+    pl.scan_csv(raw_data)    
+      .sort("State")  
+      .select(["State", "RegionName"])
 )
+initial_data = pl.scan_csv(raw_data).sort("State")
+initial_df = initial_data.collect()
+data = q.collect()             
 
-data = q.collect() 
+con.execute("""
+                CREATE TABLE IF NOT EXISTS city_geocode (
+                    id INTEGER,
+                    city_state TEXT,
+                    latitude REAL,
+                    longitude REAL
+                )
+            """)
+con.commit()
+
+con.sql("CREATE TABLE redfin_data AS SELECT * FROM initial_df")
+
+
+count = 0
 for rows in data.iter_rows(named=True):
+        
         city_state = f"{rows['RegionName']}, {rows['State']}"
         location = geolocator.geocode(city_state)
         if location:
-            print(location.latitude, location.longitude)
+            try:
+                con.execute(f"INSERT INTO city_geocode (id, city_state, latitude, longitude) VALUES ({count}, '{city_state}', {location.latitude}, {location.longitude})")
+                con.commit()
+                print(f"Inserted: {city_state} -> ({location.latitude}, {location.longitude})")
+            except Exception as e:
+                print(f"Error inserting {city_state}: {e}")
         else:
             print(f"No Latitude, Longitude Data for: {city_state}")
-
-# if __name__ == "__main__":
-#    create_latlong()
+        count += 1
