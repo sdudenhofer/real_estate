a5aee3247c3d689a500a9c88144bdb8ec475371d
Seth Dudenhofer <sdudenhofer@gmail.com>
2025-10-01 17:38:22 -0500

    added logic for better error catching

--- 1-db_insert.py
+++ 1-db_insert.py
@@ -1,9 +1,11 @@
 import polars as pl
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 from sqlmodel import SQLModel, Field
 from dotenv import load_dotenv
 import os
 import duckdb
+from time import sleep
 import pyarrow as pa
 
 load_dotenv()
@@ -36,7 +38,12 @@ con.sql("CREATE TABLE IF NOT EXISTS redfin_data AS SELECT * FROM initial_df")
 count = 0
 for rows in data.iter_rows(named=True):
         city_state = f"{rows['RegionName']}, {rows['State']}"
-        location = geolocator.geocode(city_state)
+        try:
+            location = geolocator.geocode(city_state)
+        except GeocoderTimedOut as g:
+            print(f"GeocoderTimedOut for {city_state}: {g}")
+            location = None
+            sleep(5)
         if location:
             try:
                 con.execute(f"INSERT INTO city_geocode (id, city_state, latitude, longitude) VALUES ({count}, '{city_state}', {location.latitude}, {location.longitude})")
